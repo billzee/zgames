325c90ef48125d9b975e7cac89276289f2892889
Guilherme Zordan <guilhermezordan@outlook.com>
2017-08-28 15:23:56 -0300

    trocado database para pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170825162742) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
     t.string "title"
     t.string "image"
